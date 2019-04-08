18c726e190adcf611e8c58b9ae4ebb2b25ef4008
brunodamasco <brunodamasco1@gmail.com>
2019-04-08 11:45:10 -0300

    08/04/2019 - Criação e envio do projeto

--- src/java/br/com/senac/entidade/Produto.java
+++ src/java/br/com/senac/entidade/Produto.java
@@ -16,7 +16,6 @@ import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.websocket.ClientEndpoint;
 
 /**
  *
